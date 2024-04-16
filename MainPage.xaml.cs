a66f6dd2d4eb6ace40549f9d26a037ee5d7d835c
Chris Moldenauer <56985918+MoldyHacker@users.noreply.github.com>
2024-04-15 21:39:30 -0500

    added date picker

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,30 +2,29 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
+            birthdatePicker.Date = DateTime.Now;
         }
 
         private void OnCheckClicked(object sender, EventArgs e)
         {
-            if (int.TryParse(ageEntry.Text, out int age))
+            DateTime birthdate = birthdatePicker.Date;
+            DateTime dateOf21stBirthday = birthdate.AddYears(21);
+            int yearsUntil21 = dateOf21stBirthday.Year - DateTime.Now.Year;
+
+            if (DateTime.Now > dateOf21stBirthday)
             {
-                if (age <= 21)
-                {
-                    int yearsLeft = 21 - age;
-                    resultLabel.Text = $"Years until you can legally buy alcohol: {yearsLeft}";
-                }
-                else
-                {
-                    resultLabel.Text = "You are already old enough to buy alcohol!";
-                }
+                resultLabel.Text = "You are already old enough to buy alcohol!";
+            }
+            else if (yearsUntil21 == 0)
+            {
+                resultLabel.Text = "You will turn 21 this year!";
             }
             else
             {
-                resultLabel.Text = "Please enter a valid age.";
+                resultLabel.Text = $"Years until you can legally buy alcohol: {yearsUntil21}";
             }
         }
     }
