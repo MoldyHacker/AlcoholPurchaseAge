fd781d85bfd1c8a00c007a015567a8952c2eb0ad
Chris Moldenauer <56985918+MoldyHacker@users.noreply.github.com>
2024-04-15 21:29:11 -0500

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AlcoholPurchaseAge
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace AlcoholPurchaseAge
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCheckClicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(ageEntry.Text, out int age))
+            {
+                if (age <= 21)
+                {
+                    int yearsLeft = 21 - age;
+                    resultLabel.Text = $"Years until you can legally buy alcohol: {yearsLeft}";
+                }
+                else
+                {
+                    resultLabel.Text = "You are already old enough to buy alcohol!";
+                }
+            }
+            else
+            {
+                resultLabel.Text = "Please enter a valid age.";
+            }
+        }
+    }
+
+}
