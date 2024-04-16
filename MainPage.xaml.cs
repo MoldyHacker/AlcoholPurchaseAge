91138beae01bd855e633f6e224bd5737c4f63eb8
Chris Moldenauer <56985918+MoldyHacker@users.noreply.github.com>
2024-04-15 21:41:48 -0500

    added country check

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -10,21 +10,43 @@
 
         private void OnCheckClicked(object sender, EventArgs e)
         {
-            DateTime birthdate = birthdatePicker.Date;
-            DateTime dateOf21stBirthday = birthdate.AddYears(21);
-            int yearsUntil21 = dateOf21stBirthday.Year - DateTime.Now.Year;
+            if (countryPicker.SelectedIndex == -1)
+            {
+                resultLabel.Text = "Please select your country.";
+                return;
+            }
 
-            if (DateTime.Now > dateOf21stBirthday)
+            string selectedCountry = countryPicker.SelectedItem.ToString();
+            DateTime birthdate = birthdatePicker.Date;
+            DateTime now = DateTime.Now;
+            int legalDrinkingAge = GetLegalDrinkingAge(selectedCountry);
+            DateTime legalAgeDate = birthdate.AddYears(legalDrinkingAge);
+
+            int yearsUntilLegal = legalAgeDate.Year - now.Year;
+            if (now > legalAgeDate)
             {
                 resultLabel.Text = "You are already old enough to buy alcohol!";
             }
-            else if (yearsUntil21 == 0)
+            else if (yearsUntilLegal == 0)
             {
-                resultLabel.Text = "You will turn 21 this year!";
+                resultLabel.Text = "You will be old enough to buy alcohol this year!";
             }
             else
             {
-                resultLabel.Text = $"Years until you can legally buy alcohol: {yearsUntil21}";
+                resultLabel.Text = $"Years until you can legally buy alcohol in {selectedCountry}: {yearsUntilLegal}";
+            }
+        }
+
+        private int GetLegalDrinkingAge(string country)
+        {
+            switch (country)
+            {
+                case "United States": return 21;
+                case "Canada": return 19;  // Note: This is variable by province, most are 19
+                case "United Kingdom": return 18;
+                case "Germany": return 16; // Beer and wine, 18 for spirits
+                case "Japan": return 20;
+                default: return 18;
             }
         }
     }
